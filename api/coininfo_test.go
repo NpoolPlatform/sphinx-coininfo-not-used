b40f2590fa3345c2aba17720d212eea66ef8ddfe
Cyvadra <dj@lifesdk.com>
2021-11-17 16:06:24 +0800

    Implement APIs and UTs.

--- api/coininfo_test.go
+++ api/coininfo_test.go
@@ -12,11 +12,33 @@ import (
 	testinit "github.com/NpoolPlatform/sphinx-coininfo/pkg/test-init"
 )
 
+var tmpCoinInfo *npool.CoinInfoRow
+
 func init() {
 	err := testinit.Init()
 	if err != nil {
 		panic(err)
 	}
+	tmpCoinInfo = &npool.CoinInfoRow{
+		CoinType:  -2,
+		IsPresale: false,
+		Name:      "Unit Test",
+		Unit:      "UT",
+	}
+}
+
+func TestGetCoinInfo(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	cli := resty.New()
+	_, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.GetCoinInfoRequest{
+			CoinType: tmpCoinInfo.CoinType,
+		}).
+		Get("http://localhost:50130/v1/coin/single")
+	assert.Nil(t, err)
 }
 
 func TestGetCoinInfos(t *testing.T) {
@@ -27,7 +49,7 @@ func TestGetCoinInfos(t *testing.T) {
 	_, err := cli.R().
 		SetHeader("Content-Type", "application/json").
 		SetBody(npool.GetCoinInfosRequest{}).
-		Get("http://localhost:50130/v0/coin/infos")
+		Get("http://localhost:50130/v1/coin/infos")
 	assert.Nil(t, err)
 }
 
@@ -39,9 +61,25 @@ func TestRegisterCoin(t *testing.T) {
 	_, err := cli.R().
 		SetHeader("Content-Type", "application/json").
 		SetBody(npool.RegisterCoinRequest{
-			Name: "Filecoin",
-			Unit: "FIL",
+			CoinType: tmpCoinInfo.CoinType,
+			Name:     tmpCoinInfo.Name,
+			Unit:     tmpCoinInfo.Unit,
 		}).
-		Post("http://localhost:50130/v0/coin/register")
+		Post("http://localhost:50130/v1/coin/register")
+	assert.Nil(t, err)
+}
+
+func TestSetCoinPresale(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	cli := resty.New()
+	_, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.SetCoinPresaleRequest{
+			CoinType:  tmpCoinInfo.CoinType,
+			IsPresale: false,
+		}).
+		Post("http://localhost:50130/v1/coin/presale")
 	assert.Nil(t, err)
 }
