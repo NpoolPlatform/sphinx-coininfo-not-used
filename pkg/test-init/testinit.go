2cbc8b522212e1ed0d0512b9cce112418966d7ae
Cyvadra <dj@lifesdk.com>
2021-11-22 17:10:16 +0800

    bugfix testinit.go

--- pkg/test-init/testinit.go
+++ pkg/test-init/testinit.go
@@ -25,6 +25,7 @@ func Init() error {
 	configPath := fmt.Sprintf("%s/../../cmd/%v", path.Dir(myPath), appName)
 
 	err := app.Init(servicename.ServiceName, "", "", "", configPath, nil, nil,
+		config.ServiceNameToNamespace(servicename.ServiceName),
 		config.ServiceNameToNamespace(mysqlconst.MysqlServiceName),
 		config.ServiceNameToNamespace(redisconst.RedisServiceName),
 		config.ServiceNameToNamespace(rabbitmqconst.RabbitMQServiceName))
