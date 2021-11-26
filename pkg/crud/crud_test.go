eacdaa074c180d5577594c0c438f56dd7e40c98a
dengjie <dj@lifesdk.com>
2021-11-26 14:08:40 +0800

    add crud test

---
+++ pkg/crud/crud_test.go
@@ -0,0 +1,58 @@
+package crud
+
+import (
+	"context"
+	"testing"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/sphinx-coininfo/pkg/db/ent"
+	testinit "github.com/NpoolPlatform/sphinx-coininfo/pkg/test-init"
+	"github.com/NpoolPlatform/sphinx-coininfo/pkg/testaio"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCRUD(t *testing.T) {
+	if testaio.RunByGithub() {
+		return
+	}
+	if !testaio.InitAlready {
+		assert.Nil(t, testinit.Init())
+		testaio.InitAlready = true
+	}
+	ctx := context.Background()
+	coinInfo, err := CreateCoin(ctx, entCoinInfo)
+	logger.Sugar().Infof("CreateCoin result: %v", coinInfo)
+	AbortWhenError(t, err)
+	coinInfo, err = GetInfo(ctx, entCoinInfo.ID.String())
+	logger.Sugar().Infof("GetInfo result: %v", coinInfo)
+	AbortWhenError(t, err)
+	coinInfos, err := GetInfos(ctx)
+	logger.Sugar().Infof("GetInfos result: %v", coinInfos)
+	AbortWhenError(t, err)
+	coinInfo, err = UpdatePreSale(ctx, entCoinInfo)
+	logger.Sugar().Infof("UpdatePreSale result: %v", coinInfo)
+	AbortWhenError(t, err)
+}
+
+func LogWhenError(err error) {
+	if err != nil {
+		logger.Sugar().Warn(err)
+	}
+}
+
+func AbortWhenError(t *testing.T, err error) {
+	if err != nil {
+		logger.Sugar().Warn(err)
+	}
+	assert.Nil(t, err)
+}
+
+var entCoinInfo = &ent.CoinInfo{
+	ID:         uuid.MustParse("8fbcbdc2-25ea-4ff0-b049-9d2f4c8ab646"),
+	CoinTypeID: 1,
+	Name:       "FIL",
+	Unit:       "FIL",
+	IsPresale:  false,
+	LogoImage:  "",
+}
