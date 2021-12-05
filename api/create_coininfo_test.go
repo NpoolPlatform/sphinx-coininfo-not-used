74adb1535f23296a05c8c37d259a42823e98ed42
linuxpizi <1360447275@qq.com>
2021-12-05 16:57:54 +0800

    cancel coininfo

--- api/create_coininfo_test.go
+++ api/create_coininfo_test.go
@@ -1,41 +1,8 @@
 package api
 
 import (
-	"encoding/json"
-	"os"
-	"strconv"
 	"testing"
-
-	"github.com/NpoolPlatform/message/npool/coininfo"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"gopkg.in/resty.v1"
 )
 
 func TestCreateCoinInfo(t *testing.T) {
-	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
-		return
-	}
-
-	var (
-		cli      = resty.New()
-		coinInfo coininfo.CreateCoinInfoResponse
-	)
-
-	resp, err := cli.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(coininfo.CreateCoinInfoRequest{
-			PreSale: false,
-			Name:    "FIL",
-			Unit:    "FIL",
-			Logo:    "",
-		}).
-		Post("http://localhost:36759/v1/create/coininfo")
-	if assert.Nil(t, err) {
-		assert.Equal(t, 200, resp.StatusCode())
-		err := json.Unmarshal(resp.Body(), &coinInfo)
-		if assert.Nil(t, err) {
-			assert.NotEqual(t, coinInfo.GetID(), uuid.UUID{}.String())
-		}
-	}
 }
