d5a584263de44773bb8a1cc0f9de9f9167b4d534
linuxpizi <1360447275@qq.com>
2021-12-06 11:20:36 +0800

    check args

--- api/create_coininfo.go
+++ api/create_coininfo.go
@@ -6,26 +6,15 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	npool "github.com/NpoolPlatform/message/npool/coininfo"
 	"github.com/NpoolPlatform/sphinx-coininfo/pkg/crud/coininfo"
-	"github.com/NpoolPlatform/sphinx-coininfo/pkg/db/ent"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
+// CreateCoinInfo support duplicate
 func (s *Server) CreateCoinInfo(ctx context.Context, in *npool.CreateCoinInfoRequest) (*npool.CreateCoinInfoResponse, error) {
 	if in.GetName() == "" {
 		logger.Sugar().Errorf("CreateCoinInfo check Name is empty")
 		return nil, status.Error(codes.InvalidArgument, "Name empty")
-	}
-
-	_, err := coininfo.GetCoinInfoByName(ctx, in.GetName())
-	if !ent.IsNotFound(err) {
-		logger.Sugar().Errorf("CreateCoinInfo check Name: %v already exist", in.GetName())
-		return nil, status.Errorf(codes.AlreadyExists, "Name: %v already exist", in.GetName())
-	}
-
-	if err != nil {
-		logger.Sugar().Errorf("CreateCoinInfo call GetCoinInfoByName error %v", err)
-		return nil, status.Error(codes.Internal, "internal server error")
 	}
 
 	if in.GetUnit() == "" {
@@ -33,7 +22,7 @@ func (s *Server) CreateCoinInfo(ctx context.Context, in *npool.CreateCoinInfoReq
 		return nil, status.Error(codes.InvalidArgument, "Unit empty")
 	}
 
-	err = coininfo.CreateCoinInfo(ctx, &npool.CoinInfo{
+	err := coininfo.CreateCoinInfo(ctx, &npool.CoinInfo{
 		PreSale: in.GetPreSale(),
 		Name:    in.GetName(),
 		Unit:    in.GetUnit(),
--- api/update_coininfo.go
+++ api/update_coininfo.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	npool "github.com/NpoolPlatform/message/npool/coininfo"
 	"github.com/NpoolPlatform/sphinx-coininfo/pkg/crud/coininfo"
+	"github.com/NpoolPlatform/sphinx-coininfo/pkg/db/ent"
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -17,12 +18,24 @@ func (s *Server) UpdateCoinInfo(ctx context.Context, in *npool.UpdateCoinInfoReq
 		return nil, status.Error(codes.InvalidArgument, "ID empty")
 	}
 
-	if _, err := uuid.Parse(in.GetID()); err != nil {
+	id, err := uuid.Parse(in.GetID())
+	if err != nil {
 		logger.Sugar().Errorf("UpdateCoinInfo check ID not a valid uuid")
 		return nil, status.Error(codes.InvalidArgument, "ID invalid")
 	}
 
-	_, err := coininfo.UpdateCoinInfoByID(ctx, in.GetPreSale(), in.GetLogo(), in.GetID())
+	_, err = coininfo.GetCoinInfoByID(ctx, id)
+	if ent.IsNotFound(err) {
+		logger.Sugar().Errorf("UpdateCoinInfo call GetCoinInfoByID ID: %v not found", in.GetID())
+		return nil, status.Errorf(codes.NotFound, "ID: %v not found", in.GetID())
+	}
+
+	if err != nil {
+		logger.Sugar().Errorf("UpdateCoinInfo call GetCoinInfoByID error %v", err)
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	_, err = coininfo.UpdateCoinInfoByID(ctx, in.GetPreSale(), in.GetLogo(), in.GetID())
 	if err != nil {
 		logger.Sugar().Errorf("UpdateCoinInfo call UpdateCoinInfoByID error %v", err)
 		return nil, status.Error(codes.Internal, "internal server error")
